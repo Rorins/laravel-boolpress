5cc43ceefeff099e666fd31811221a00c872260d
Aurora Grippaudo <aurora268@hotmail.it>
2022-01-31 15:38:54 +0100

    setting up

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//HOME FRONT
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+//ROTTE PER AUTENTICAZIONE
+Auth::routes();
+
+//ROTTE AREA ADMIN
+Route::get('/home', 'HomeController@index')->name('home');
